e94ec93f654c7f8f78c9f1b563acaae8a139b0bc
AkiraTorres <torres.thg@gmail.com>
2024-02-09 18:00:36 -0300

    fix: editado nome de alguns componentes do Dialog

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -29,6 +29,10 @@ export function NewNoteCard() {
         }
     }
 
+    function handleExit() {
+        setShouldShowOnboarding(false);
+    }
+
     return (
         <Dialog.Root>
             <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -41,12 +45,12 @@ export function NewNoteCard() {
 
             </Dialog.Trigger>
             <Dialog.Portal>
-                <Dialog.DialogOverlay className="inset-0 fixed bg-black/50"></Dialog.DialogOverlay>
-                <Dialog.DialogContent className="fixed left-1/2 top-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+                <Dialog.Overlay className="inset-0 fixed bg-black/50"></Dialog.Overlay>
+                <Dialog.Content className="fixed left-1/2 top-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
 
-                <Dialog.DialogClose className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+                <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
                     <X className="size-5" />
-                </Dialog.DialogClose>
+                </Dialog.Close>
 
                 <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
                     <div className="flex flex-1 flex-col gap-3 p-5">
@@ -75,7 +79,7 @@ export function NewNoteCard() {
                         Salvar nota
                     </button>
                 </form>
-                </Dialog.DialogContent>
+                </Dialog.Content>
             </Dialog.Portal>
         </Dialog.Root>
     );
--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -25,12 +25,12 @@ export function NoteCard({ note }: NoteCardProps) {
           </Dialog.Trigger>
 
           <Dialog.Portal>
-            <Dialog.DialogOverlay className="inset-0 fixed bg-black/50"></Dialog.DialogOverlay>
-            <Dialog.DialogContent className="fixed left-1/2 top-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+            <Dialog.Overlay className="inset-0 fixed bg-black/50"></Dialog.Overlay>
+            <Dialog.Content className="fixed left-1/2 top-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
 
-              <Dialog.DialogClose className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+              <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
                 <X className="size-5" />
-              </Dialog.DialogClose>
+              </Dialog.Close>
 
               <div className="flex flex-1 flex-col gap-3 p-5">
 
@@ -48,7 +48,7 @@ export function NoteCard({ note }: NoteCardProps) {
                 Deseja <span className="text-red-400 group-hover:underline">apagar essa nota?</span>
               </button>
 
-            </Dialog.DialogContent>
+            </Dialog.Content>
           </Dialog.Portal>
         </Dialog.Root>
     );
